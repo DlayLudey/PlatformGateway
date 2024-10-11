646f3236b777c1d418879df8f6d9347df928d2ff
DasDaOne <mr.ars355@GMAIL.COM>
2024-10-11 13:14:19 +0300

    Added Yandex Platform

--- Assets/CarrotHood.PlatformDeps.Yandex/Runtime/Scripts/YandexPlatform.cs
+++ Assets/CarrotHood.PlatformDeps.Yandex/Runtime/Scripts/YandexPlatform.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using Kimicu.YandexGames;
 using UnityEngine;
 
@@ -16,12 +17,71 @@ namespace CarrotHood.PlatformDeps.Yandex
 		}
 
 		public override IEnumerator Init(PlatformBuilder builder)
-#if !YandexOverride
 		{
-			yield return null;
+			yield return YandexGamesSdk.Initialize();
+			yield return Billing.Initialize();
+			
+			builder.AddPayments(new PaymentsYandex());
+
+			yield return Cloud.Initialize();
+			builder.AddStorage(new StorageYandex());
+			
+			Advertisement.Initialize();
+			builder.AddAdvertisement(new AdvertisementYandex(Settings.interstitialCooldown));
 		}
-#else
-;
-#endif
+	}
+
+	public class PaymentsYandex : IPayments
+	{
+		public bool isSupported { get; } = true;
+		public void ConsumePurchase(string productToken, Action onSuccessCallback = null, Action<string> onErrorCallback = null)
+		{
+			Billing.ConsumeProduct(productToken, onSuccessCallback, onErrorCallback);
+		}
+
+		public void GetPurchases(Action<object> onSuccessCallback, Action<string> onErrorCallback = null)
+		{
+			Billing.GetPurchasedProducts(onSuccessCallback, onErrorCallback);
+		}
+
+		public void Purchase(string productId, Action<object> onSuccessCallback = null, Action<string> onErrorCallback = null)
+		{
+			Billing.PurchaseProduct(productId, onSuccessCallback, onErrorCallback);
+		}
+	}
+
+	public class AdvertisementYandex : AdvertisementBase
+	{
+		public AdvertisementYandex(int platformInterstitialCooldown) : base(platformInterstitialCooldown) { }
+
+		public override void CheckAdBlock(Action<bool> callback) => callback?.Invoke(AdBlock.Enabled);
+
+		public override void ShowBanner(Dictionary<string, object> options = null) { }
+
+		public override void HideBanner() { }
+
+		protected override void ShowInterstitialInternal(Action onOpen, Action onClose, Action<string> onError)
+		{
+			Advertisement.ShowInterstitialAd(onOpen, onClose, onError);
+		}
+
+		public override void ShowRewarded(Action onRewarded, Action<string> onError)
+		{
+			Advertisement.ShowVideoAd(onRewardedCallback: onRewarded, onErrorCallback: onError);
+		}
+	}
+
+	public class StorageYandex : IStorage
+	{
+		public void GetValue(string key, Action<string> onSuccess, Action<string> onError)
+		{
+			onSuccess?.Invoke(Cloud.GetValue(key, ""));
+		}
+
+		public void SetValue(string key, string value, Action onSuccess, Action<string> onError)
+		{
+			Cloud.SetValue(key, value, true);
+			onSuccess?.Invoke();
+		}
 	}
 }
