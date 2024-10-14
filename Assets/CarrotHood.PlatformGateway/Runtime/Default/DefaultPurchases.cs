3d3f18c51e3c0c614181827d10c1c66260122092
DasDaOne <mr.ars355@GMAIL.COM>
2024-10-14 13:50:10 +0300

    Update DefaultPurchases.cs

--- Assets/CarrotHood.PlatformGateway/Runtime/Default/DefaultPurchases.cs
+++ Assets/CarrotHood.PlatformGateway/Runtime/Default/DefaultPurchases.cs
@@ -5,6 +5,7 @@ namespace CarrotHood.PlatformGateway
 {
 	public class DefaultPayments : IPayments
 	{
+		public IPayments.Product[] Products { get; }
 		public bool isSupported => false;
 
 		public void ConsumePurchase(string productToken, Action onSuccessCallback = null, Action<string> onErrorCallback = null) { }
