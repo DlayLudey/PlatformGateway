5ea8bcda0956bf87c18ab7eadb42885b6fc027bc
DasDaOne <mr.ars355@GMAIL.COM>
2024-12-20 12:16:09 +0300

    My bad

--- Assets/CarrotHood.PlatformGateway/Runtime/Default/DefaultPlatform.cs
+++ Assets/CarrotHood.PlatformGateway/Runtime/Default/DefaultPlatform.cs
@@ -5,19 +5,13 @@ namespace CarrotHood.PlatformGateway
 {
 	public class DefaultPlatform : PlatformBase
 	{
-		[SerializeField] private Product[] products;
-		
 		public override PlatformType Type => PlatformType.Default;
 
 		public override string Language => "ru";
 
 		public override IEnumerator Init(PlatformBuilder builder)
 		{
-			builder.Advertisement = new DefaultAdvertisement(interstitialCooldown);
-			builder.Storage = new DefaultStorage(saveCooldown);
-			yield return builder.Storage.Initialize();
-			builder.Payments = new DefaultPayments(products, builder.Storage);
-			builder.Social = new DefaultSocial();
+			yield return null;
 		}
 	}
 }
--- Assets/CarrotHood.PlatformGateway/Runtime/EditorPlatform.cs
+++ Assets/CarrotHood.PlatformGateway/Runtime/EditorPlatform.cs
@@ -8,13 +8,18 @@ namespace CarrotHood.PlatformGateway
 	public class EditorPlatform : PlatformBase
 	{
 		[SerializeField] private string editorLang;
+		[SerializeField] private Product[] products;
 
 		public override PlatformType Type => default;
 		public override string Language => editorLang;
 	
 		public override IEnumerator Init(PlatformBuilder builder)
 		{
-			yield break;
+			builder.Advertisement = new DefaultAdvertisement(interstitialCooldown);
+			builder.Storage = new DefaultStorage(saveCooldown);
+			yield return builder.Storage.Initialize();
+			builder.Payments = new DefaultPayments(products, builder.Storage);
+			builder.Social = new DefaultSocial();
 		}
 	}	
 }
