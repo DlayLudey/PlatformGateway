7b5d7827c460ee939e8efea2052ab8bfcc9a0207
DasDaOne <mr.ars355@GMAIL.COM>
2025-01-23 12:47:14 +0300

    Update PlaygamaPlatform.cs

--- Assets/CarrotHood.PlatformGateway.Playgama/Runtime/Scripts/PlaygamaPlatform.cs
+++ Assets/CarrotHood.PlatformGateway.Playgama/Runtime/Scripts/PlaygamaPlatform.cs
@@ -20,10 +20,6 @@ namespace CarrotHood.PlatformGateway.Playgama
 
 		public override IEnumerator Init(PlatformBuilder builder)
 		{
-			// Why does it have to be MonoBehaviour again?
-			Bridge bridge = new GameObject("Bridge").AddComponent<Bridge>();
-			DontDestroyOnLoad(bridge);
-			
 			builder.Storage = new StoragePlaygama(saveCooldown);
 
 			yield return builder.Storage.Initialize();
